54c25c197fc7b6426fade997d366a121b72808b0
JBJ <maystar5987@gmail.com>
2023-09-14 17:35:01 +0900

    package

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,19 @@
 // const {username, age} = {username:"Alice", age:30}
 // username, age
 
-import { appName, greet, user } from "./module.js";
+import { appName, greet, user } from "./module";
 // 디푤트 모듈
 // 자동완성으로 파일명과 모듈명을 동일하게 해줌
 // import module from "./module";
-import metadata from "./module.js";
+import metadata from "./module";
 
-const name = "Javascripts";
+const name = "Javascripts!!";
+
 console.log(greet(`${name}-${appName}
 -${metadata.version}-${metadata.creator}
 -${user.name}-${user.age}`)
 );
+
+document.getElementById("root").innerHTML = greet(`${name}-${appName}
+-${metadata.version}-${metadata.creator}
+-${user.name}-${user.age}`);
