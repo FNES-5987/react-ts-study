0d32299892ed9eb72a0e1d806adfd158f4e956ef
JBJ <maystar5987@gmail.com>
2023-09-18 16:56:24 +0900

    todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,13 @@
-// 리액트는 컴포넌트 단위로 개발
-// 컴포넌트(component) : 페이지, 세부화면, 위젯, 아이콘, 버튼
-// : 재사용이 가능한 UI 조각
-// : 템플릿(틀) + 작동코드
-
-// 리액트 컴포넌트는 파일명과 함수명을 파스칼케이스로 작성
-// 원래 리액트 컴포넌트는 class/function 2가지 형태였고,
-// 기본이 class 형식이었음
-// 원래 자바스크립트에서 생성자함수 new Person()는 원래 파스칼케이스
-// function Person() { name; age; function printInfo() {} }
-
-// 리액트 컴포넌트 :
-// JSX Element를 반환하는 함수
-
-import react_icon from "./assets/react-icon.png";
-import intro from "./assets/intro.mp4";
-import WelcomeMessage from "./components/WelcomeMessage";
+import Counter from "./components/Counter";
+import Todo from "./components/Todo";
 
 const App = () => {
-  // React.createElement(component, props, ...children)
-  // React.createElement("div", null, "Hello, React!!")
   return (
-    <div>
-      {/* 이미지 사용 */}
-      <img src={react_icon} alt="react icon" height={16} />
-      <span>Hello, React!!</span>
-      {/* 비디오 사용 */}
-      <div>
-        <video width={480} height={270} controls loop autoPlay muted>
-          <source src={intro} type="video/mp4" />
-        </video>
-      </div>
-      <WelcomeMessage />
-    </div>
+    <>
+      <Counter />
+      <hr />
+      <Todo />
+    </>
   );
 };
 
--- src/components/WelcomeMessage.tsx
+++ src/components/WelcomeMessage.tsx
@@ -1,7 +1,14 @@
+interface WelcomeMessageProps {
+  name: string;
+}
+
 // 리엑트에서 컴포넌트는 함수인데,
 // JSX.Element를 반환하는 함수
-const WelcomeMessage = () => {
-  const name = "ChatGPT";
+
+// React 컴포넌트는 매개변수를 객체로 받아야한다
+// React Props: 함수의 객체형태 매개변수
+const WelcomeMessage = ({ name = "ChatGPT" }: WelcomeMessageProps) => {
+  // const name = "ChatGPT";
 
   // <></> : Fragment
   // JSX : js 가상DOM 객체/ HTML 스타일로 작성
@@ -14,6 +21,9 @@ const WelcomeMessage = () => {
       {/* 바인딩(binding) : 데이터(변수값)를 템플릿에 연결 */}
       {/* 리액트는 단방향 바인딩만 지원한다 (코드 -> 템플릿) */}
       {/* {name} : jsx의 자바스크립트 표현식 */}
+
+      {/* jsx : 속성 = {값} */}
+      {/* jsx : 속성 = {객체} */}
       <h1 style={{ color: "green" }}>Welcome, {name}!</h1>
       <p>This is an example of JSX in React.</p>
     </div>
